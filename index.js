7c85843ccb2dd2f15983bdef82e1f64bf1b09cfe
AndreasGrip <andreas.grip@gmail.com>
2023-11-18 13:56:45 +0100

    Add support for UTF16/32  booth big and little endian

--- index.js
+++ index.js
@@ -15,9 +15,26 @@ function BomStrippingStream(options) {
 	this._transform = function(chunk, encoding, done) {
 		if (atStart) {
 			atStart = false; //only runs this at the beginning
+			// UTF8
 			if (chunk[0] == 0xEF && chunk[1] == 0xBB && chunk[2] == 0xBF) {
 				chunk = chunk.slice(3);
 			}
+			// UTF16-LE
+			if (chunk[0] == 0xFF && chunk[1] == 0xFE) {
+				chunk = chunk.slice(2);
+			}
+			// UTF16-BE
+			if (chunk[0] == 0xFE && chunk[1] == 0xFF) {
+				chunk = chunk.slice(2);
+			}
+			// UTF32-LE
+			if (chunk[0] == 0xFE && chunk[1] == 0xFF && chunk[2] == 0x00 && chunk[3] == 0x00) {
+				chunk = chunk.slice(4);
+			}
+			// UTF32-BE
+			if (chunk[0] == 0x00 && chunk[1] == 0x00 && chunk[2] == 0xFE && chunk[3] == 0xFF) {
+				chunk = chunk.slice(4);
+			}			
 		}
 		this.push(chunk);
 		return done();
